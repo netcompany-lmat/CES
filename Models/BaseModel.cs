b89211e22f6e2aad35b2e773c3ecb05123d88e5f
Łukasz Kajetan Zalewski <lukz@netcompany.com>
2022-08-25 10:18:05 +0200

    Add initial data models

---
+++ Models/BaseModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ces.Models;
+
+public class BaseModel
+{
+    public Guid Id { get; set; }
+}
---
+++ Models/ParcelType.cs
@@ -0,0 +1,8 @@
+﻿namespace ces.Models;
+
+public class ParcelType : BaseModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double ExtraFee { get; set; }
+}
