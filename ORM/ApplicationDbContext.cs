4c4d675d923f1e37749faf10c083319578a9bffe
Łukasz Kajetan Zalewski <lukz@netcompany.com>
2022-08-25 10:18:14 +0200

    Add base DbContext setup

---
+++ ORM/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ces.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ces.ORM;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+    {
+    }
+
+    public DbSet<City> Cities { get; set; }
+
+    public async Task<int> SaveChanges()
+    {
+        return await base.SaveChangesAsync();
+    }
+}
