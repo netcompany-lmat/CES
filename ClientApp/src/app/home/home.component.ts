84a43f7c2153d17d9846218138d2603b2fc382ee
OmnixPL <47388572+OmnixPL@users.noreply.github.com>
2022-08-25 13:37:08 +0200

    Frontend navbar, city selector (#11)
    Co-authored-by: Kacper Kula <kula@netcompany.com>

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { City } from '../interfaces/city';
+import { cities } from './temp-data'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  allCities = cities;
+
+  fromCity?: City;
+  toCity?: City;
+
+  fromText = "From:";
+  toText = "To:";
 }
