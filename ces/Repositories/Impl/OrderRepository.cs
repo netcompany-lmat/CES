1da39fc25bee33c881dd234f39313b45caa5547b
Ali Zedan <42241471+ali123321123@users.noreply.github.com>
2022-08-25 22:53:45 +0200

    Feat: Added Service and Repository for order (#20)
    * Feat: Added Service and Repository for order
    
    * Changed structure
    
    * merged main
    
    * deleted extra file csperoj
    
    * changed path for delete order
    
    * Add AddTransient<AppDbContext> and update InsertOrder
    
    Co-authored-by: Ali Zedan <alized@netcompany.com>
    Co-authored-by: Łukasz Kajetan Zalewski <lukz@netcompany.com>

---
+++ ces/Repositories/Impl/OrderRepository.cs
@@ -0,0 +1,55 @@
+﻿using ces.Models;
+using ces.ORM;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+
+namespace ces.Repositories.Impl
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext _applicationDbContext;
+
+        public OrderRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public void DeleteOrder(Guid orderId)
+        {
+            var entity = new Order()
+            {
+                Id = orderId
+            };
+            _applicationDbContext.Orders.Remove(entity);
+            _applicationDbContext.SaveChanges();
+        }
+
+        public async Task<Order> GetOrderById(Guid id)
+        {
+             return await _applicationDbContext.
+                Orders.FirstOrDefaultAsync(order => order.Id == id);
+        }
+
+     
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return  await  _applicationDbContext.Orders.ToListAsync();
+        }
+
+        public async Task InsertOrder(Order order)
+        {
+
+            await _applicationDbContext.AddAsync(order);
+            _applicationDbContext.SaveChanges();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            var UpdateOrder =  _applicationDbContext.Orders.Attach(order);
+            UpdateOrder.State = EntityState.Modified;
+            _applicationDbContext.SaveChanges();
+        }
+       
+    }
+}
---
+++ ces/Services/Impl/OrderService.cs
@@ -0,0 +1,40 @@
+﻿using ces.Models;
+using ces.Repositories;
+
+namespace ces.Services.Impl
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;   
+
+        public OrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public void DeleteOrder(Guid orderId)
+        {
+            _orderRepository.DeleteOrder(orderId);
+        }
+
+        public async Task<Order> GetOrderById(Guid Id)
+        {
+           return  await _orderRepository.GetOrderById(Id);
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _orderRepository.GetOrders();
+        }
+
+        public async Task InsertOrder(Order order)
+        {
+            await _orderRepository.InsertOrder(order);
+        }
+
+        public  void UpdateOrder(Order order)
+        {
+             _orderRepository.UpdateOrder(order);
+        }
+    }
+}
