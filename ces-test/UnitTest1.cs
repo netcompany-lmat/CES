b23dc56be1008d360619bda6cb9808e7084545b3
Mateusz Łąpieś <lmat@netcompany.com>
2022-08-25 21:04:29 +0200

    reorganization

---
+++ ces-test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ces_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
