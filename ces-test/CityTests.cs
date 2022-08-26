53582198826da1e50c5b7c6cd0cf67d916ee1665
bidh <bidh@netcompany.com>
2022-08-26 02:00:41 +0200

    Adding unit test classes

---
+++ ces-test/CityTests.cs
@@ -0,0 +1,52 @@
+﻿using ces.Controllers;
+using ces.Models;
+using ces.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ces_test
+{
+    public class CityTests
+    {
+        Mock<ILogger<CityController>> loggerMock;
+        Mock<ICityService>? cityServiceMock;
+        [SetUp]
+        public void Setup()
+        {
+            loggerMock = new Mock<ILogger<CityController>>();
+        }
+
+        [Test]
+        public async Task ReturnCities()
+        {
+            cityServiceMock = new Mock<ICityService>();
+
+
+            List<City> cities = new()
+            {
+                new City()
+                {
+                   Name = "London"
+                },
+                new City()
+                {
+                   Name = "Copenhagen"
+                },
+            };
+
+            cityServiceMock.Setup(x => x.GetCities())
+                .ReturnsAsync(cities);
+
+            var cityController = new CityController(loggerMock.Object, cityServiceMock.Object);
+            var response = await cityController.GetCities();
+
+
+            Assert.AreEqual(cities.Count, response.Value.Count);
+        }
+    }
+}
---
+++ ces-test/OrderTests.cs
@@ -0,0 +1,42 @@
+using ces.Controllers;
+using ces.Models;
+using ces.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ces_test
+{
+    public class OrderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public async Task ReturnsOrderByOrderId()
+        {
+            var logger = new Mock<ILogger<OrderController>>();
+            var orderStub = new Mock<IOrderService>(logger);
+
+            Guid validOrderId = new();
+
+            Order order = new()
+            {
+
+            };
+
+            orderStub.Setup(x => x.GetOrderById(validOrderId))
+                .ReturnsAsync(new Order()
+                {
+                                       
+                });
+
+            var orderController = new OrderController(logger.Object, orderStub.Object);
+            var response = await orderController.GetOrderById(validOrderId);
+
+            Assert.AreSame("","");            
+        }
+    }
+}
